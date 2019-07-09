aa6c64664d54868ad41f1b38ec381b6e62f29a5d
Joosung Park <admin@slowmotion.dev>
2019-07-09 22:39:57 +0900

    add value and text

--- src/static/index.ts
+++ src/static/index.ts
@@ -1,7 +1,31 @@
 document.title = 'FT-Island';
 
-const app: HTMLDivElement = document.createElement('div');
-app.id = 'app';
-app.innerText = 'Hello!';
+document.body.appendChild<HTMLDivElement>(((): HTMLDivElement => {
+    const appContainer: HTMLDivElement = document.createElement('div');
+    appContainer.id = 'app-container';
 
-document.body.appendChild<HTMLDivElement>(app);
+    appContainer.appendChild<HTMLDivElement>(((): HTMLDivElement => {
+        const valueTextContainer: HTMLDivElement = document.createElement('div');
+        valueTextContainer.id = 'value-text-container';
+
+        valueTextContainer.appendChild<HTMLDivElement>(((): HTMLDivElement => {
+            const valueText: HTMLDivElement = document.createElement('div');
+            valueText.id = 'value-text';
+            valueText.innerText = '100T';
+
+            return valueText;
+        })());
+
+        valueTextContainer.appendChild<HTMLDivElement>(((): HTMLDivElement => {
+            const valueTextInfo: HTMLDivElement = document.createElement('div');
+            valueTextInfo.id = 'value-text-info';
+            valueTextInfo.innerText = 'Hello!';
+
+            return valueTextInfo;
+        })());
+
+        return valueTextContainer;
+    })());
+
+    return appContainer;
+})());
